a9ecf11a7e1d71ca060b51a6899f915da361b2b2
Mark Skelton <mdskelton99@gmail.com>
2020-05-19 20:40:51 -0500

    Add another test

--- src/__testfixtures__/function-component.output.js
+++ src/__testfixtures__/function-component.output.js
@@ -1,11 +1,11 @@
-import PropTypes from "prop-types";
-import React from "react";
+import PropTypes from "prop-types"
+import React from "react"
 
 export function MyComponent() {
-  return <span />;
+  return <span />
 }
 
 MyComponent.propTypes = {
-  foo: PropTypes.number,
   bar: PropTypes.string.isRequired,
-};
+  foo: PropTypes.number,
+}
---
+++ src/__tests__/transform.spec.js
@@ -0,0 +1,4 @@
+const { defineTest } = require("jscodeshift/dist/testUtils")
+
+defineTest(__dirname, "function-component")
+defineTest(__dirname, "complex-props")
