6a3ea6b9089225839efe502fb52982d996c026aa
Mark Skelton <mdskelton99@gmail.com>
2020-05-19 21:06:07 -0500

    Complex props

---
+++ src/__testfixtures__/complex-props.input.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types"
+import React from "react"
+
+export function MyComponent(props) {
+  return <span />
+}
+
+MyComponent.propTypes = {
+  optionalArray: PropTypes.array,
+  optionalBool: PropTypes.bool,
+  optionalFunc: PropTypes.func,
+  optionalNumber: PropTypes.number,
+  optionalObject: PropTypes.object,
+  optionalString: PropTypes.string,
+  optionalSymbol: PropTypes.symbol,
+  optionalNode: PropTypes.node,
+  optionalElement: PropTypes.element,
+  optionalElementType: PropTypes.elementType,
+  optionalEnum: PropTypes.oneOf(["News", "Photos"]),
+  optionalUnion: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  optionalArrayOf: PropTypes.arrayOf(PropTypes.number),
+  optionalObjectOf: PropTypes.objectOf(PropTypes.number),
+  optionalObjectWithShape: PropTypes.shape({
+    optionalProperty: PropTypes.string,
+    requiredProperty: PropTypes.number.isRequired,
+  }),
+  optionalObjectWithStrictShape: PropTypes.exact({
+    optionalProperty: PropTypes.string,
+    requiredProperty: PropTypes.number.isRequired,
+  }),
+  requiredFunc: PropTypes.func.isRequired,
+  requiredAny: PropTypes.any.isRequired,
+}
---
+++ src/__testfixtures__/complex-props.output.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+type Props = {
+  optionalArray?: unknown[]
+  optionalBool?: boolean
+  optionalFunc?(...args: unknown[]): unknown
+  optionalNumber?: number
+  optionalObject?: object
+  optionalString?: string
+  optionalSymbol?: symbol
+  optionalNode?: React.ReactNode
+  optionalElement?: React.ReactElement
+  optionalElementType?: React.ElementType
+  optionalEnum?: "News" |"Photos"
+  optionalUnion?: string | number
+  optionalArrayOf?: number[]
+  optionalObjectOf?: Record<string, number>
+  optionalObjectWithShape?: {
+    optionalProperty?: string
+    requiredProperty: number
+  }
+  optionalObjectWithStrictShape: {
+    optionalProperty?: string
+    requiredProperty: number
+  }
+  requiredFunc(...args: unknown[]): unknown
+  requiredAny: any
+}
+
+export function MyComponent(props: Props) {
+  return <span />
+}
