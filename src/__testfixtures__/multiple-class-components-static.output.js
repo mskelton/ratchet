e133233a0f36c2b252126e477f171869eee89f6a
Mark Skelton <mdskelton99@gmail.com>
2020-05-20 23:27:56 -0500

    Multiple static components

---
+++ src/__testfixtures__/multiple-class-components-static.output.js
@@ -0,0 +1,23 @@
+import React from "react"
+
+type ComponentAProps = {
+  a: string,
+  b?: number
+};
+
+export class ComponentA extends React.Component<ComponentAProps> {
+  render() {
+    return <span />
+  }
+}
+
+type ComponentBProps = {
+  c?: unknown[],
+  d: object
+};
+
+export class ComponentB extends React.Component<ComponentBProps> {
+  render() {
+    return <span />
+  }
+}
