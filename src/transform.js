cdc9082371116b4ca2edcfc4dfe0e516eb9b8911
Mark Skelton <mdskelton99@gmail.com>
2020-05-20 23:19:04 -0500

    Support classes

--- src/transform.js
+++ src/transform.js
@@ -17,8 +17,8 @@ function removeImport(j, source) {
  * @param {import('jscodeshift').JSCodeshift} j
  * @param {import('jscodeshift').Collection} source
  */
-function removeFunctionPropTypes(j, source) {
-  const types = source
+function findPropTypes(j, source) {
+  return source
     .find(j.AssignmentExpression)
     .filter(
       (path) =>
@@ -26,15 +26,38 @@ function removeFunctionPropTypes(j, source) {
         path.value.left.property.type === "Identifier" &&
         path.value.left.property.name === "propTypes"
     )
+}
+
+/**
+ * @param {import('jscodeshift').JSCodeshift} j
+ * @param {import('jscodeshift').Collection} source
+ */
+function findStaticPropTypes(j, source) {
+  return source
+    .find(j.ClassProperty)
+    .filter((path) => path.value.static && path.value.key.name === "propTypes")
+}
+
+/**
+ * @param {import('jscodeshift').JSCodeshift} j
+ * @param {import('jscodeshift').Collection} source
+ */
+function collectPropTypes(j, source) {
+  const types = findPropTypes(j, source)
+  const staticTypes = findStaticPropTypes(j, source)
 
   // Store the types to survive after we remove the propTypes
-  const results = types.paths().map((path) => ({
-    component: path.value.left.object.name,
-    propTypes: parsePropTypes(j, path.value.right.properties),
-  }))
+  const results = types
+    .paths()
+    .concat(staticTypes.paths())
+    .map((path) => ({
+      component: path.value.left.object.name,
+      propTypes: parsePropTypes(j, path.value.right.properties),
+    }))
 
-  // Remove the propTypes assignment expression
+  // Remove the propTypes assignment expression and static propTypes
   types.remove()
+  staticTypes.remove()
 
   return results
 }
@@ -42,9 +65,9 @@ function removeFunctionPropTypes(j, source) {
 /**
  * @param {import('jscodeshift').JSCodeshift} j
  * @param {import('jscodeshift').Collection} source
+ * @param {{ component: string, propTypes: import('jscodeshift').TSPropertySignature[] }[]} types
  */
-function replaceFunctionPropTypes(j, source) {
-  const types = removeFunctionPropTypes(j, source)
+function addFunctionTSTypes(j, source, types) {
   const components = source
     .find(j.FunctionDeclaration)
     // Ignore functions without propTypes
@@ -75,8 +98,33 @@ function replaceFunctionPropTypes(j, source) {
 /**
  * @param {import('jscodeshift').JSCodeshift} j
  * @param {import('jscodeshift').Collection} source
+ * @param {{ component: string, propTypes: import('jscodeshift').TSPropertySignature[] }[]} types
  */
-function replaceStaticPropTypes(j, source) {}
+function addClassTSType(j, source, types) {
+  const components = source
+    .find(j.ClassDeclaration)
+    // Ignore classes without propTypes
+    .filter((path) => types.find((t) => t.component === path.value.id.name))
+
+  components.forEach((path) => {
+    const componentName = path.value.id.name
+    const { propTypes } = types.find((t) => t.component === componentName)
+    const typeName = components.length === 1 ? "Props" : `${componentName}Props`
+
+    // Add the TS types before the function
+    path.parentPath.insertBefore(
+      j.tsTypeAliasDeclaration(
+        j.identifier(typeName),
+        j.tsTypeLiteral(propTypes)
+      )
+    )
+
+    // Add the TS types to the React.Component super class
+    path.value.superTypeParameters = j.tsTypeParameterInstantiation([
+      j.tsTypeReference(j.identifier(typeName)),
+    ])
+  })
+}
 
 /**
  * @param {import('jscodeshift').FileInfo} fileInfo
@@ -86,10 +134,15 @@ module.exports = function (fileInfo, api) {
   const j = api.jscodeshift
   const source = api.jscodeshift(fileInfo.source)
 
+  // Remove the prop-types import from the top of the file
   removeImport(j, source)
-  replaceFunctionPropTypes(j, source)
-  // replaceClassPropTypes(j, source)
-  // replaceStaticPropTypes(j, source)
+
+  // Collect prop types from assignment expressions and static prop types
+  const types = collectPropTypes(j, source).concat()
+
+  // Add TS types to functions and classes
+  addFunctionTSTypes(j, source, types)
+  addClassTSType(j, source, types)
 
   return source.toSource()
 }
