14318dcff17bda5e62ac4743c12fcf432f080d53
Mark Skelton <mdskelton99@gmail.com>
2020-05-20 22:58:57 -0500

    Add tests

--- src/__tests__/transform.spec.js
+++ src/__tests__/transform.spec.js
@@ -1,5 +1,7 @@
 const { defineTest } = require("jscodeshift/dist/testUtils")
 
 defineTest(__dirname, "transform", null, "function-component")
+defineTest(__dirname, "transform", null, "class-component")
+defineTest(__dirname, "transform", null, "class-component-static")
 defineTest(__dirname, "transform", null, "multiple-components")
 defineTest(__dirname, "transform", null, "complex-props")
