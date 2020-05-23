54007a3aac0682e568b1c1a544e024665b56c2d9
Mark Skelton <mdskelton99@gmail.com>
2020-05-22 23:53:40 -0500

    Specs

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,12 +1,7 @@
-// module.exports = {
-//   preset: "jest-playwright-preset",
-//   // setupFilesAfterEnv: ["expect-playwright"],
-//   // testRunner: "jest-circus/runner",
-//   // testEnvironment: "./config/environment.js",
-//   testTimeout: 35 * 1000,
-// }
-
 module.exports = {
   preset: "jest-playwright-preset",
+  setupFilesAfterEnv: ["expect-playwright"],
+  testRunner: "jest-circus/runner",
+  testEnvironment: "./config/environment.js",
   testTimeout: 35 * 1000,
 }
