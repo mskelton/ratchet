a701e195ed5eecde42bb84dc3418d0be2a995129
mskelton <mskelton@users.noreply.github.com>
2020-05-23 14:11:58 +0000

    Apply formatting changes

--- e2e/jest.config.js
+++ e2e/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "jest-playwright-preset",
   setupFilesAfterEnv: ["expect-playwright"],
-  testRunner: "jest-circus/runner",
   testEnvironment: "./config/environment.js",
+  testRunner: "jest-circus/runner",
   testTimeout: 35 * 1000,
 }
