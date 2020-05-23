e370f3962990da7dae3a76a2894647a1b4ee7145
Mark Skelton <mdskelton99@gmail.com>
2020-05-22 20:52:30 -0500

    Styles

--- web/src/components/ConfigBar.svelte
+++ web/src/components/ConfigBar.svelte
@@ -3,7 +3,19 @@
 </script>
 
 <style>
+  .container {
+    background-color: #383838;
+    border-bottom: 1px solid #1b2224;
+    color: #ccc;
+    padding: 8px 16px;
+  }
 
+  h1 {
+    font-size: 22px;
+    margin: 0;
+  }
 </style>
 
-<div>Config</div>
+<div class="container">
+  <h1>PropTypes to TS</h1>
+</div>
