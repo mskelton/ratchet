3a7c91d396d6db97640df1228d0d22aab5041fee
Mark Skelton <mdskelton99@gmail.com>
2020-05-22 21:13:53 -0500

    Transform when changing options

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,21 +1,31 @@
 <script>
   import Editor from "./components/Editor.svelte"
-  import ConfigBar from "./components/ConfigBar.svelte"
+  import Header from "./components/Header.svelte"
   import { inputSnippet, outputSnippet } from "./editor/snippets"
   import transform from "../../transform"
   import jscodeshift from "jscodeshift"
 
+  const source = { current: inputSnippet }
   let options = {}
   let output = outputSnippet
+
+  function doTransform(source) {
+    output = transform({ source }, { jscodeshift }, options)
+  }
 
   function handleOptionChange(event) {
     options = {
       "preserve-prop-types": event.detail.preservePropTypes
     }
+
+    // Since the options have changed, we need to re-compile the source
+    doTransform(source.current)
   }
 
   function handleChange(event) {
-    output = transform({ source: event.detail.value }, { jscodeshift }, options)
+    // Store the source for use if the options are changed
+    source.current = event.detail.value
+    doTransform(source.current)
   }
 </script>
 
@@ -33,7 +43,7 @@
 </style>
 
 <div class="container">
-  <ConfigBar on:change={handleOptionChange} />
+  <Header on:change={handleOptionChange} />
 
   <div class="editors">
     <Editor value={inputSnippet} on:change={handleChange} />
