fa58b03d793f97299523a9ceb7ef8acef11356ff
Mark Skelton <mdskelton99@gmail.com>
2020-05-23 22:47:59 -0500

    Update test

--- __tests__/transform.spec.js
+++ __tests__/transform.spec.js
@@ -9,16 +9,35 @@ defineTest(__dirname, "transform", null, "multiple-class-components-static")
 defineTest(__dirname, "transform", null, "function-and-class")
 defineTest(__dirname, "transform", null, "custom-validator")
 
-// Options
+// --preserve-prop-types
 defineTest(
   __dirname,
   "transform",
   { "preserve-prop-types": true },
   "preserve-prop-types"
 )
+
+// --preserve-prop-types=all
 defineTest(
   __dirname,
   "transform",
   { "preserve-prop-types": "all" },
+  "preserve-prop-types"
+)
+
+// --preserve-prop-types=custom
+defineTest(
+  __dirname,
+  "transform",
+  { "preserve-prop-types": "custom-validators" },
+  "preserve-custom-validators"
+)
+
+// --preserve-prop-types=custom
+// When no custom validators exist, should match --preserve-prop-types=all
+defineTest(
+  __dirname,
+  "transform",
+  { "preserve-prop-types": "custom-validators" },
   "preserve-prop-types"
 )
