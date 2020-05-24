56f77a03866fba8f1037e3e2661eb701216cae71
Mark Skelton <mdskelton99@gmail.com>
2020-05-24 17:57:35 -0500

    Fix storage

--- web/src/storage.js
+++ web/src/storage.js
@@ -6,6 +6,9 @@ export const storage = new Proxy(
   {},
   {
     get: (_, key) => localStorage.getItem(`option:${key}`) || defaults[key],
-    set: (_, key, value) => localStorage.getItem(`option:${key}`, value),
+    set(_, key, value) {
+      localStorage.setItem(`option:${key}`, value)
+      return true
+    },
   }
 )
