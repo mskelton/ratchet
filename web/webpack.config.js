68420a3ff7e3ac9b1cbfb3419d3db0db7fd3690a
Mark Skelton <mdskelton99@gmail.com>
2020-05-27 23:17:29 -0500

    Update

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -36,9 +36,7 @@ module.exports = {
       },
       {
         loader: "babel-loader",
-        options: {
-          rootMode: "upward",
-        },
+        options: { rootMode: "upward" },
         test: /\.tsx?$/,
       },
       {
