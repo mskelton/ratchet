3a5173a5e4a2d1992b245efcd8b3288df08fb43a
Mark Skelton <mdskelton99@gmail.com>
2021-08-20 12:50:18 -0500

    Fix command

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -2,7 +2,8 @@ import { PlaywrightTestConfig } from "@playwright/test"
 
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
-  retries: 2,
+  reporter: process.env.CI ? "dot" : "line",
+  retries: process.env.CI ? 2 : 0,
   use: {
     baseURL: process.env.ENVIRONMENT_URL || "http://localhost:8080",
     screenshot: "only-on-failure",
