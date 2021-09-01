684ba8c7a03b136c5ce1dcac7571e6277d7724bf
Mark Skelton <mdskelton99@gmail.com>
2021-08-31 19:27:07 -0500

    Recast doesn't indent comments properly, but it's better than not supporting it

--- __testfixtures__/comments.output.js
+++ __testfixtures__/comments.output.js
@@ -1,16 +1,16 @@
 import React from "react"
 
 interface MyComponentProps {
-  /**
-   * A string with a
-   * wrapping comment.
-   * @example "foo"
-   */
-  bar: string;
-  /**
-   * Some function
-   */
-  foo?(...args: unknown[]): unknown;
+ /**
+  * A string with a
+  * wrapping comment.
+  * @example "foo"
+  */
+ bar: string;
+ /**
+  * Some function
+  */
+ foo?(...args: unknown[]): unknown;
 }
 
 export function MyComponent(props: MyComponentProps) {
